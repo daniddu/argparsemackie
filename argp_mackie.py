ce5f129381a4dfe6d5c9bd06e41553b946fa9e10
dani limbeck <dani.limbeck@gmx.at>
2023-06-11 20:00:52 +0200

    with output file argument

--- argp_mackie.py
+++ argp_mackie.py
@@ -2,10 +2,11 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser(description='write changed content in new file')
-parser.add_argument('--inp_file', required=True, help=('write here name of the file'))
+parser.add_argument('--inp_file', help=('write here name of the input file'), required=True)
+parser.add_argument('--outp_file', help=('write here name of the output file'), required=True)
 args = parser.parse_args()
 
-def replace_voc(inp_file):
+def replace_voc(inp_file, outp_file):
 
     with open(inp_file, encoding='utf-8') as poem_file:
         material = poem_file.read()
@@ -14,11 +15,11 @@ def replace_voc(inp_file):
         for voc, voc_num in dicto.items():
             material = material.replace(voc, str(voc_num))  
     
-    with open('new_mackie.txt', 'a') as nf:
+    with open(outp_file, 'a') as nf:
         nf.write(material + '\n' + '\n')
         nf.close()
-    with open('new_mackie.txt') as nf:
+    with open(outp_file) as nf:
         new_content = nf.read()
         return new_content
 
-print(replace_voc(args.inp_file))
+print(replace_voc(args.inp_file, args.outp_file))
